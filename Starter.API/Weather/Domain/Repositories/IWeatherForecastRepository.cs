fb9767ed2a308a30c6a9fd762957cb5af102f85b
Diego Albitres <diego.albitres@gmail.com>
2022-06-03 22:02:54 -0500

    style: add spacing between declarations

--- Starter.API/Weather/Domain/Repositories/IWeatherForecastRepository.cs
+++ Starter.API/Weather/Domain/Repositories/IWeatherForecastRepository.cs
@@ -2,8 +2,12 @@ namespace Starter.API.Weather.Domain.Repositories;
 
 public interface IWeatherForecastRepository {
   Task<IEnumerable<WeatherForecast>> ListAll();
+
   Task Add(WeatherForecast forecast);
+
   Task<WeatherForecast> FindById(int id);
+
   Task Update(WeatherForecast forecast);
+
   Task Remove(WeatherForecast forecast);
 }
