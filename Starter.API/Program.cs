3b86eac2056dc692976b28bcff89b19c6a790780
Diego Albitres <diego.albitres@gmail.com>
2022-06-04 05:10:42 -0500

    refactor: migrate to Pomelo's MySQL connector
    * Docs: https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql
    * Seems to (actually) work better with migrations than the official connector

--- Starter.API/Program.cs
+++ Starter.API/Program.cs
@@ -9,25 +9,26 @@ using Starter.API.Weather.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add database connection
-
 var connectionString = builder.Configuration["DbConnectionString"];
+var serverVersion = ServerVersion.AutoDetect(connectionString);
+var logLevel = builder.Environment.IsProduction() ? LogLevel.Warning : LogLevel.Information;
+var enableDebugInfo = !builder.Environment.IsProduction();
 
 builder.Services.AddDbContext<AppDbContext>(
-  options => options.UseMySQL(connectionString)
-    .LogTo(Console.WriteLine, LogLevel.Information)
-    .EnableSensitiveDataLogging(!builder.Environment.IsProduction())
-    .EnableDetailedErrors(!builder.Environment.IsProduction())
+  options => options.UseMySql(connectionString, serverVersion)
+    .LogTo(Console.WriteLine, logLevel)
+    .EnableSensitiveDataLogging(enableDebugInfo)
+    .EnableDetailedErrors(enableDebugInfo)
 );
 
 // Add lowercase routes
-
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 // Dependency Injection configuration
@@ -36,7 +37,6 @@ builder.Services.AddScoped<IForecastService, ForecastService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 // AutoMapper configuration
-
 builder.Services.AddAutoMapper(typeof(ModelToResourceProfile), typeof(ResourceToModelProfile));
 
 var app = builder.Build();
